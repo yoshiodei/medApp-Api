016602d88aab100546b16e9f79d25cdac6ca32d2
Yoshi Odei <yoshiodei@yahoo.com>
2023-03-03 09:21:56 -0800

    add middlewares

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const app = express();
+const userRoute = require('./routes/user');
+require('dotenv').config();
+mongoose.set('strictQuery', true);
 
-app.get((req,res)=>{  res.json({message:"edey job"})  })
+const PORT = process.env.port || 3003;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use('/user', userRoute);
+app.use('/patient', userRoute);
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, ()=>{
+    console.log('connected to database!!');
+});
+
+app.listen(PORT, ()=>{ 
+    console.log(`listening to port ${PORT}!!`); 
+  });
